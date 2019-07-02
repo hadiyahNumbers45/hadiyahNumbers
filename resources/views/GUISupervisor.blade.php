17004cb80a5c912f089b2c6dfca370209c5c184c
unknown <manal@hadiyah.org.sa>
2019-07-02 09:30:20 +0300

    f

---
+++ resources/views/GUISupervisor.blade.php
@@ -0,0 +1,20 @@
+<?php
+require 'C:\Users\HADIYAH\Desktop\hadiyah\hadiyahNymbers\resources\views\header.blade.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    .GUIObserverFormsContent{padding: 300px;}
+  </style>
+  <title>form</title>
+</head>
+<body>
+<div class="GUIObserverFormsContent">
+
+</div>
+</body>
+</html>
+<?php
+require 'C:\Users\HADIYAH\Desktop\hadiyah\hadiyahNymbers\resources\views\footer.blade.php';
+?>
