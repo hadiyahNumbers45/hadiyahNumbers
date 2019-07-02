c4b6407fe82db2b8c00bcc943c0e95e9b70e3736
unknown <manal@hadiyah.org.sa>
2019-07-02 08:20:25 +0300

    f

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/GUIProgram', function () {
+    return view('GUIProgram');
+});
+Route::get('/GUIObserver', function () {
+    return view('GUIObserver');
+});
+Route::get('/GUIObserverForms', function () {
+    return view('GUIObserverForms');
+});
+Route::get('/GUISupervisor', function () {
+    return view('GUISupervisor');
+});
+Route::get('/GUIObserverInfo', function () {
+    return view('GUIObserverInfo');
+});
+Route::get('/GUIAdmin', function () {
+    return view('GUIAdmin');
+});
+Route::get('/GUIadminProgramInfo', function () {
+    return view('GUIadminProgramInfo');
+});
+Route::get('/GUIit', function () {
+    return view('GUIit');
+});
