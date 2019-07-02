4ad88fc9a1c68d594b850d1fec535b341ac7ee28
unknown <manal@hadiyah.org.sa>
2019-07-02 11:44:00 +0300

    commit #1

--- resources/views/GUIObserverForms.blade.php
+++ resources/views/GUIObserverForms.blade.php
@@ -1,20 +1,7 @@
-<?php
-require 'C:\Users\HADIYAH\Desktop\hadiyah\hadiyahNymbers\resources\views\header.blade.php';
-?>
-<!DOCTYPE html>
-<html>
-<head>
-  <style>
-    .GUIObserverFormsContent{padding: 300px;}
-  </style>
-  <title>form</title>
-</head>
-<body>
-<div class="GUIObserverFormsContent">
-
-</div>
-</body>
-</html>
-<?php
-require 'C:\Users\HADIYAH\Desktop\hadiyah\hadiyahNymbers\resources\views\footer.blade.php';
-?>
+@extends('templates.hadiyah')
+@section('title')
+Page title
+@endsection
+@section('content')
+<h1>Page content</h1>
+@endsection
--- resources/views/templates/hadiyah.blade.php
+++ resources/views/templates/hadiyah.blade.php
@@ -1,10 +1,64 @@
+
 <!DOCTYPE html>
 <html>
 <head>
   <style>
-  footer{
- background-color:  #009973;
- padding:30px 0px;
+  body{
+font-family:Roboto;
+}
+
+.menu
+{
+background-color: black;
+border-bottom: 4px solid #4f5467;
+width:100%;
+height: auto;
+padding: 0 10px;
+position: fixed;
+margin: 0px;
+z-index: 1;
+opacity: 0.9;
+}
+
+.menu  .navbar-nav > .active > a
+{
+background-color : #4f5467;
+color: white;
+font-weight: bold;
+}
+
+.menu  .navbar-nav >  li >  a
+{
+font-size: 13px;
+color: white;
+padding: 10px 35px;
+
+}
+.menu  .navbar-nav >  li >  a:hover
+{
+background-color: #4f5467;
+}
+
+.navbar-header {
+padding: 10px;
+}
+.navbar-header > a {
+font-family: 'Ubuntu Condensed', sans-serif;
+padding: 0px;
+margin: 0px;
+text-decoration: none;
+color: white;
+font-size: 13px;
+padding: 5px 3px;
+}
+.navbar-header > a:hover
+{
+text-decoration: none;
+color: #04A3ED;
+}
+footer{
+background-color:  #009973;
+padding:30px 0px;
 }
 
 footer ul li{
@@ -43,19 +97,35 @@ margin-right: 15px;
 font-size:14px;
 }
   </style>
-	<link rel="stylesheet" type="text/css" href="">
+  <title>@yield('title')</title>
 </head>
 <body>
 <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <!------ Include the above in your HEAD tag ---------->
+<header>
+<!--header top-->
+<div class="menu">
+    <div class="container-fluid">
+    <div class="navbar-header">
+      <a href="#"><span class="glyphicon glyphicon-user"></span> Email: .... </a>
+      <a href="#"><span class="glyphicon glyphicon-phone"></span> Mobile: .... </a>
+    </div>
+    <div>
+      <ul class="nav navbar-nav navbar-right">
 
-<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> تسجيل الدخول</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+<!--end Header-->
+</header>
+<br></br></br>
+<div>
+@yield('content')
+</div>
 <footer>
  <div class="container">
    <div class="row">
